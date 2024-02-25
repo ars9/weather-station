65965801b4761b438df3b5fa0eb543ae49ddaf76
Andrei Arsenin <andrei.arsenin@hapi.one>
2024-02-26 01:11:49 +0200

    Move secrets to .env

---
+++ src/credentials.hpp
@@ -0,0 +1,31 @@
+#ifndef WIFI_SSID
+#define WIFI_SSID "defaultSSID"
+#endif
+
+#ifndef WIFI_PASSWORD
+#define WIFI_PASSWORD "defaultPassword"
+#endif
+
+#ifndef POST_URL
+#define POST_URL "https://metrics.example.com/"
+#endif
+
+#ifndef BEARER_TOKEN
+#define BEARER_TOKEN "Bearer MYTOKEN"
+#endif
+
+#ifndef MQTT_USER
+#define MQTT_USER "defaultUser"
+#endif
+
+#ifndef MQTT_PASSWORD
+#define MQTT_PASSWORD "defaultPassword"
+#endif
+
+#ifndef MQTT_URI
+#define MQTT_URI "mqtt://example.com"
+#endif
+
+#ifndef CA_CERT
+#define CA_CERT "-----BEGIN CERTIFICATE-----EXAMPLECERTIFICATE-----END CERTIFICATE-----"
+#endif
